96f7acd9aace23f4da18a9d2e52e84e2e0eece71
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-30 01:01:10 +0300

    Unit-testing my models

--- hood_watch/tests.py
+++ hood_watch/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class NeighbourhoodTest(TestCase):
+    def setUp(self):
+        self.Nai = Neighbourhood.objects.create(name='Nai')
+
+    def test_instance(self):
+        self.Nai.save()
+        self.assertTrue(isinstance(self.Nai, Neighbourhood))
+
+    def test_get_hoods(self):
+        self.Nai.save()
+        hoods = Neighbourhood.get_hoods()
+        self.assertTrue(len(hoods) > 0)
+
+
+class BusinessTest(TestCase):
+    def setUp(self):
+        self.myBusiness= Business.objects.create(name='myBusiness', email='mybusiness@gmail.com')
+
+    def test_instance(self):
+        self.myBusiness.save()
+        self.assertTrue(isinstance(self.myBusiness,Business))
